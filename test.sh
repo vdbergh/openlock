b3e0c17f407fea90f78ea99af340417b0c686da6
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2024-06-20 09:35:45 +0200

    Tweak test.sh.

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
-python test.py &
-python test.py &
-python test.py &
-python test.py &
-python test.py &
-python test.py &
+# Create stale lock file!
+touch test.lock
+
 python test.py &
 python test.py &
 python test.py &
@@ -11,4 +8,18 @@ python test.py &
 python test.py &
 python test.py &
 
+sleep 4
 
+python test.py &
+python test.py &
+python test.py &
+python test.py &
+python test.py &
+python test.py &
+
+sleep 5
+
+# Exactly one python process should be running!
+ps waux |grep python |grep test.py
+
+wait
