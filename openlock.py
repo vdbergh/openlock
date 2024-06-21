13b019a52a22633d80e69b77f4e3bb7a0df2e127
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2024-06-21 15:33:10 +0200

    Stale lock file detection using PID.
    In the lock file we write
    pid\n
    script_name\n

--- openlock.py
+++ openlock.py
@@ -1,9 +1,12 @@
 import atexit
 import logging
 import os
+import sys
 import threading
 import time
 from pathlib import Path
+
+from util import atomic_write, pid_valid
 
 __version__ = "0.0.1"
 
@@ -18,9 +21,7 @@ class Timeout(OpenLockException):
     pass
 
 
-# These deal with stale lock file detection
-_touch_period_default = 2.0
-_stale_timeout_default = 3.0
+# This deals with stale lock file detection
 _stale_race_delay_default = 0.5
 
 # This deals with acquiring locks
@@ -34,8 +35,6 @@ class FileLock:
         detect_stale=False,
         timeout=None,
         _retry_period=_retry_period_default,
-        _touch_period=_touch_period_default,
-        _stale_timeout=_stale_timeout_default,
         _stale_race_delay=_stale_race_delay_default,
     ):
         self.__lock_file = Path(lock_file)
@@ -45,31 +44,20 @@ class FileLock:
         self.__acquired = False
         self.__timer = None
         self.__retry_period = _retry_period
-        self.__touch_period = _touch_period
-        self.__stale_timeout = _stale_timeout
         self.__stale_race_delay = _stale_race_delay
         logger.debug(f"{self} created")
 
-    def __touch(self):
-        self.__lock_file.touch()
-        self.__timer = threading.Timer(self.__touch_period, self.__touch)
-        self.__timer.daemon = True
-        self.__timer.start()
-        if not self.__acquired:
-            self.__timer.cancel()
-
     def __is_stale(self):
-        if not self.__lock_file.exists():
-            return False
         try:
-            mtime = os.path.getmtime(self.__lock_file)
-        except OSError as e:
-            logger.error(
-                "Unable to get the modification time of the lock file "
-                f"{self.__lock_file}: {str(e)}"
-            )
+            with open(self.__lock_file) as f:
+                w = f.readlines()
+                pid = int(w[0])
+                name = w[1]
+        except FileNotFoundError:
             return False
-        if mtime < time.time() - self.__stale_timeout:
+        except Exception:
+            return True
+        if not pid_valid(pid, name):
             return True
         return False
 
@@ -94,19 +82,14 @@ class FileLock:
                         self.__remove_lock_file()
                         time.sleep(self.__stale_race_delay)
                 try:
-                    fd = os.open(
-                        self.__lock_file,
-                        mode=0o644,
-                        flags=os.O_WRONLY | os.O_CREAT | os.O_EXCL,
-                    )
-                    os.write(fd, str(os.getpid()).encode())
-                    os.close(fd)
+                    pid = os.getpid()
+                    name = sys.argv[0]
+                    atomic_write(self.__lock_file, f"{pid}\n{name}\n".encode())
                     atexit.register(self.__remove_lock_file)
                     logger.debug(f"{self} acquired")
                     self.__acquired = True
-                    self.__touch()
                     break
-                except FileExistsError:
+                except OSError:
                     pass
 
                 if timeout is not None and wait_time >= timeout:
@@ -142,7 +125,7 @@ class FileLock:
                 return None
             try:
                 with open(self.__lock_file) as f:
-                    return int(f.read())
+                    return int(f.read().split()[0])
             except Exception:
                 return None
 
