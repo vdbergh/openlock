8ffdfdea3cbef562a262b4703b8ee1ed22b8b273
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2024-06-23 08:06:19 +0200

    Refactor util.py.

--- util.py
+++ util.py
@@ -6,37 +6,27 @@ IS_WINDOWS = "windows" in platform.system().lower()
 
 
 def pid_valid(pid, name):
-    with ExitStack() as stack:
-        if IS_WINDOWS:
-            cmdlet = (
-                "(Get-CimInstance Win32_Process "
-                "-Filter 'ProcessId = {}').CommandLine"
-            ).format(pid)
-            p = stack.enter_context(
-                subprocess.Popen(
-                    [
-                        "powershell",
-                        cmdlet,
-                    ],
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.DEVNULL,
-                    universal_newlines=True,
-                    bufsize=1,
-                    close_fds=not IS_WINDOWS,
-                )
-            )
-        else:
-            p = stack.enter_context(
-                subprocess.Popen(
-                    # for busybox these options are undocumented...
-                    ["ps", "-f", "-a"],
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.DEVNULL,
-                    universal_newlines=True,
-                    bufsize=1,
-                    close_fds=not IS_WINDOWS,
-                )
-            )
+    if IS_WINDOWS:
+        cmdlet = (
+            "(Get-CimInstance Win32_Process " "-Filter 'ProcessId = {}').CommandLine"
+        ).format(pid)
+        cmd = [
+            "powershell",
+            cmdlet,
+        ]
+    else:
+        # for busybox these options are undocumented...
+        cmd = ["ps", "-f", "-a"]
+
+    with subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+        universal_newlines=True,
+        bufsize=1,
+        close_fds=not IS_WINDOWS,
+    ) as p:
+
         for line in iter(p.stdout.readline, ""):
             if name in line and str(pid) in line:
                 return True
