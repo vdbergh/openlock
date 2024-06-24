1657a50ac610c57ad12a47aa6e27ec4a3e218527
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2024-06-24 22:59:57 +0200

    Use again full argv[0] for name.

--- openlock.py
+++ openlock.py
@@ -161,9 +161,7 @@ class FileLock:
                 return
             name = sys.argv[0]
             name_ = name.split()
-            if len(name_) >= 1:
-                name = name_[0]
-            else:
+            if len(name_) == 0:
                 name = "python"
             self.__write_lock_file(os.getpid(), name)
             time.sleep(self.__race_delay)
