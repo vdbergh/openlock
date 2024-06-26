fa18f284ab73775b79a692e383353ae979d10fbc
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2024-06-26 07:02:54 +0200

    Do spelling check of README.md in lint.sh.

--- lint.sh
+++ lint.sh
@@ -4,3 +4,4 @@ black *.py
 isort --profile black *.py
 flake8 --max-line-length 88 __init__.py _helper.py openlock.py test.py test_openlock.py
 mdl *.md
+cat README.md | aspell -a --mode=markdown --personal=./ignore.txt |grep \&
