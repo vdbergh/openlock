a6251dc808112c1fac518d6c15300ecf2acbd7c2
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2024-06-27 10:51:00 +0200

    Experiment with "ps -f <pid>".

--- openlock.py
+++ openlock.py
@@ -40,6 +40,7 @@ def pid_valid_windows(pid, name):
 def pid_valid_posix(pid, name):
     # for busybox these options are undocumented...
     cmd = ["ps", "-f", "-A"]
+    cmd = ["ps", "-f", str(pid)]
 
     with subprocess.Popen(
         cmd,
